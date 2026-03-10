e3b9fab0122149bf40247dc7d80699054e99691d
harcastic <harsh.raj2274@gmail.com>
2026-03-10 20:47:11 +0530

    user routes

--- backend/src/modules/User/controllers/userProfile.js
+++ backend/src/modules/User/controllers/userProfile.js
@@ -3,7 +3,7 @@ import User from "../../Auth/models/user-authModel.js";
 const getProfile = async(req, res) => {
 
     try {
-        const user = await User.findOne(req.user.id).select("-password");
+        const user = await User.findById(req.user.id).select("-password");
         if(!user){
             return res.status(404).json({message : "No user exists"});
         }
@@ -20,3 +20,29 @@ const getProfile = async(req, res) => {
 }
 
 export default getProfile;
+
+export const deleteProfile = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select("-password");
+        if(!user){
+            return res.status(404).json({message : "No user exists"});
+        }    
+        console.log(user);
+
+        const deleteProfile = await User.findByIdAndDelete(req.user.id).select("-password");
+        if(!deleteProfile ){
+            return res.status(404).json({
+                message : "Accound Deletion failed"
+            })
+        }
+        console.log(deleteProfile);
+        return res.status(200).json({
+            user : deleteProfile
+        })
+    } 
+    catch (error) {
+        return res.status(500).json({
+            message : "Something went wrong"
+        })
+    }
+} 
--- backend/src/modules/User/routes/usersRoute.js
+++ backend/src/modules/User/routes/usersRoute.js
@@ -2,11 +2,12 @@ import express from 'express';
 const router = express.Router();
 
 import validateToken from '../../../middlewares/authMiddleware.js';
-import getProfile from '../controllers/userProfile.js';
+import getProfile, { deleteProfile } from '../controllers/userProfile.js';
 import updateProfile from '../controllers/updateProfile.js';
 
 // GET PROFILE
 router.get('/profile', validateToken, getProfile);
 router.put('/profile', validateToken, updateProfile);
+router.delete('/profile', validateToken, deleteProfile);
 
 export default router;
